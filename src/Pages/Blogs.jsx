f7ea56b3ed1995606d8ad1c58646c69ab791ffbf
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2025-04-28 00:45:13 +0600

    added blogs and custom error message

--- src/Pages/Blogs.jsx
+++ src/Pages/Blogs.jsx
@@ -2,9 +2,92 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            this is blogs
+        <div className='w-11/12 mx-auto'>
+            <h1 className='text-center text-5xl font-bold mt-5 mb-2'>Blogs</h1>
+            <p className='text-center text-gray-500'>Let's explore some basic concept that will make you good developer</p>
+            <div className='grid grid-rows-1 gap-3.5 mt-10 mb-10'>
+                <div className=" bg-white border rounded-lg shadow-lg p-6">
+                    <h2 className="text-lg font-semibold text-gray-800">
+                        What is useState and how does it work in React?
+                    </h2>
+                    <div className="mt-3">
+                        <p className="text-blue-600 font-medium">Answer:</p>
+                        <p className="mt-1 text-gray-600">
+                            useState is a React hook that allows you to add state management to functional components. In simple terms, it lets you create and manage variables that React will keep track of and update whenever they change.
+                        </p>
+                    </div>
+                    <div className="mt-4 text-sm text-gray-500">
+                        Added on: 24-4-2025
+                    </div>
+                </div>
+                <div className=" bg-white border rounded-lg shadow-lg p-6">
+
+                    <h2 className="text-lg font-semibold text-gray-800">
+                        What is the purpose of useEffect in React?
+                    </h2>
+                    <div className="mt-3">
+                        <p className="text-blue-600 font-medium">Answer:</p>
+                        <p className="mt-1 text-gray-600">
+                            The purpose of useEffect hook is :
+                            <ul>
+                                <li>useEffect runs a block of code after the component has rendered.</li>
+                                <li>It helps manage things outside the React ecosystem, like APIs, browser APIs, or local storage.</li>
+                            </ul>
+                        </p>
+                    </div>
+                    <div className="mt-4 text-sm text-gray-500">
+                        Added on: 24-4-2025
+                    </div>
+
+
+
+
+                </div>
+                <div className=" bg-white border rounded-lg shadow-lg p-6">
+                    <h2 className="text-lg font-semibold text-gray-800">
+                        What is a custom hook in React and when should you use one?
+                    </h2>
+                    <div className="mt-3">
+                        <p className="text-blue-600 font-medium">Answer:</p>
+                        <p className="mt-1 text-gray-600">
+                            A custom hook in React is essentially a JavaScript function that allows you to extract and reuse logic from components
+                        </p>
+                    </div>
+                    <div className="mt-4 text-sm text-gray-500">
+                        Added on: 24-4-2025
+                    </div>
+                </div>
+                <div className=" bg-white border rounded-lg shadow-lg p-6">
+                    <h2 className="text-lg font-semibold text-gray-800">
+                        Difference between controlled and uncontrolled components. Which one is better?
+                    </h2>
+                    <div className="mt-3">
+                        <p className="text-blue-600 font-medium">Answer:</p>
+                        <p className="mt-1 text-gray-600">
+                            A controlled component is one where the form data is controlled by the React state. And a controlled component is one where the form data is controlled by the React state.
+                        </p>
+                    </div>
+                    <div className="mt-4 text-sm text-gray-500">
+                        Added on: 24-4-2025
+                    </div>
+                </div>
+                <div className=" bg-white border rounded-lg shadow-lg p-6">
+                    <h2 className="text-lg font-semibold text-gray-800">
+                        Tell us something about useFormStatus() in React.
+                    </h2>
+                    <div className="mt-3">
+                        <p className="text-blue-600 font-medium">Answer:</p>
+                        <p className="mt-1 text-gray-600">
+                            The useFormStatus() hook is designed to manage and display the status of a form during submission
+                        </p>
+                    </div>
+                    <div className="mt-4 text-sm text-gray-500">
+                        Added on: 24-4-2025
+                    </div>
+                </div>
+            </div>
         </div>
+
     );
 };
 
--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
-
+import errorPic from '../assets/404NotFound.jpg'
+import { Link } from 'react-router';
+// import Navber from '../../Components/Header/Navber';
 const Error = () => {
     return (
         <div>
-            this is error page
+            
+            <div className="flex flex-col items-center justify-center min-h-[70vh] px-4 py-12 bg-gray-50 text-center">
+                <img
+                    src={errorPic}
+                    alt="404 - Page Not Found"
+                    className="max-w-md w-full mb-6"
+                />
+                <h1 className="text-3xl font-bold text-pink-600 mb-2">404 - Page Not Found</h1>
+                <p className="text-gray-700 mb-6">
+                    Oops! The page you're looking for doesn’t exist.
+                </p>
+                <Link to="/" className="bg-green-600 hover:bg-green-700 text-white px-5 py-2 rounded-md font-medium transition">
+                    Go Back Home
+                </Link>
+            </div>
         </div>
     );
 };
